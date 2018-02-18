c7608d3581e17417fa1ed26320669daebf297928
shuwen.zh22@gmail.com <shuwen.zh22@gmail.com>
2018-02-18 16:30:36 -0600

    Update password validation in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,24 @@
 class User < ApplicationRecord
 	
+	PASSWORD_FORMAT = /\A
+	  (?=.{8,})          # Must contain 8 or more characters
+	  (?=.*\d)           # Must contain a digit
+	  (?=.*[a-z])        # Must contain a lower case character
+	  (?=.*[A-Z])        # Must contain an upper case character
+	  (?=.*[[:^alnum:]]) # Must contain a symbol
+	/x
+
 	has_many :collections, dependent: :destroy	# deleting user will delete the collections
 	has_many :ratings	# can rate the records in their collections
 
 	validates :username, :password, presence: true	
-	validates :username, uniqueness: {
-		case_sensitive: false}
+
+	validates :username, 
+		uniqueness: { case_sensitive: false}
+
+	validates :password, 
+		length: { minimum: 8 },
+		format: { with: PASSWORD_FORMAT },
+		confirmation: true
 
 end
