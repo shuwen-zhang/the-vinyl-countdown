dd071ec0c1e065fd59b5238df2bb8033aca9f07c
shuwen.zh22@gmail.com <shuwen.zh22@gmail.com>
2018-02-19 11:50:52 -0600

    Add Collection model

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,24 @@
+class CollectionsController < ApplicationController
+
+  # def index
+
+  # def new
+
+  def create
+   
+  end
+
+  # def edit
+  
+
+  # def show
+
+  def update
+    
+  end
+
+  def destroy
+   
+  end
+
+end
