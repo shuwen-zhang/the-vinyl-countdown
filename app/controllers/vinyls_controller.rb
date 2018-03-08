5fddffd2a7382c60cae685cc3c42c2f03d61f2d2
shuwen.zh22@gmail.com <shuwen.zh22@gmail.com>
2018-03-07 18:19:05 -0600

    Update vinyls_controller.rb

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -40,7 +40,7 @@ class VinylsController < ApplicationController
   
 
   def show
-    if session[:user_id] != nil and User.exists?(:user_id)
+    if session[:user_id] != nil 
       user = User.find_by(:id => session[:user_id])
       @collections = user.collections.order('name')
     end
