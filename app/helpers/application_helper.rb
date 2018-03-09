ffb16e8ce07e6631c47ce1a4427f413cd010b7b4
shuwen.zh22@gmail.com <shuwen.zh22@gmail.com>
2018-03-09 12:05:33 -0600

    Take care of broken or empty image_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
+
+	def print_image(url)
+		url = Rails.application.assets.find_asset(url).nil? ? nil : url
+		image_tag(url || 'na.png', :class => "img-fluid circular_image")
+	end
+
 	
 end
